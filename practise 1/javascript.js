acbda7a835a0f3a0e7bde9c39248018ff22a82bf
SYED ALI ABBAS <syedaliabbasshahmajeed@gmail.com>
2020-11-05 20:20:00 +0500

    smarter way to learn javascript 2nd commit

--- practise 1/javascript.js
+++ practise 1/javascript.js
@@ -1,1 +1,18 @@
-alert('Hello World')
+alert(name);
+
+var name = 'Syed Ali Abbas';
+name = 'usama';
+console.log(name);
+
+
+var num1 = 4;
+var num2 = 4;
+var result=num1+num2;
+console.log(result);
+
+
+
+nu1 = 9;
+nu2 = 2;
+var r = nu1%nu2;
+console.log(r);
